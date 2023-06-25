85128186aaeaa8bf7f13cc03bc18928bfee65fca
MatkowskiTeyla <137518841+MatkowskiTeyla@users.noreply.github.com>
2023-06-25 10:53:10 +0700

    Resolve conflicts

---
+++ sun_waterfall_elephant.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# This is the main script for our meditation app.
+# It will guide a user through a series of calming breaths and visualizations
+# so that they can relax and become more mindful. 
+
+# Set up our app
+print "Welcome to our meditation app!\n";
+print "We'll begin with a few calming breaths.\n";
+
+# Ask the user to take 3 deep breaths
+print "Take a deep breath in, and count to 3.\n";
+print "Hold it for 3 seconds.\n";
+print "Exhale, and count to 3.\n";
+
+# Ask the user to repeat this process
+print "Let's do that two more times.\n";
+print "Take a deep breath in, and count to 3.\n";
+print "Hold it for 3 seconds.\n";
+print "Exhale, and count to 3.\n";
+
+# Ask the user to relax their body
+print "Now, let's focus on relaxing your body.\n";
+print "Start by tensing your feet for 5 seconds.\n";
+print "Now, release your feet, and take 5 more breaths.\n";
+
+# Ask the user to focus on their breathing
+print "Let's focus on your breathing.\n";
+print "Breathe in deep, and exhale slowly.\n";
+print "Let your breath be your guide for the next 5 minutes.\n";
+
+# Ask the user to visualize a calming image
+print "Now, let's visualize a calming image.\n";
+print "Focus on a peaceful place - it can be real or imagined.\n";
+print "Concentrate on the feel of the place - imagine the warmth of the sun, or the smell of the ocean.\n";
+print "Let the image be our guide and relax your body and mind.\n";
+
+# Ask the user to take one last deep breath
+print "Take one last deep breath in, and exhale.\n";
+print "How do you feel?\n";
+
+# The app is now complete
+print "Thanks for using our meditation app!\n";
+print "We hope that you enjoyed your time of relaxation and mindfulness.\n";
